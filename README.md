c91cfb6d039a5083f74e8340449d4d3a296b5464
deelawor <deelaworbibisadiyah@gmail.com>
2019-11-15 11:03:43 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fqp8sg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fqp8sg)
---
+++ index.js
@@ -0,0 +1,8 @@
+//-first console message
+    console.log("Hello world")
+    
+// first window alert    
+    function myfunction(){
+       alert("Pop Up");
+    }
+    
